068d4cd578176f2e67b20822fceb7acce922810f
GabiiF <gabriela.fernandez.92@live.com.ar>
2025-02-05 19:30:29 -0300

    creo array e implemento la funcion agregar amigos

--- app.js
+++ app.js
@@ -1,2 +1,30 @@
 /* El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. 
  Aquí deberás desarrollar la lógica para resolver el problema.*/
+let amigos = [];
+
+
+/*function asignarElementoTexto(elemento,texto) {
+    let elementoHTML = document.getElementById(elemento);
+    elementoHTML.innerHTML = texto;
+}*/
+function agregarAmigo() {
+    // captura el valor de entrada
+    console.log("lista: ", amigos);
+    let nombre = document.getElementById("amigo").value;
+    // validar la entrada
+    if (nombre === "" || nombre === " "){
+        alert("Por favor, inserte un nombre.");
+    }else{
+        // almaceno los nombres
+        amigos.push(nombre);
+        console.log("lista: ", amigos);
+        // limpiar
+        limpiarInput();
+    }
+}
+//asignarElementoTexto("amigo");
+
+function limpiarInput() {
+    // El query selector por Id
+    document.querySelector("#amigo").value = "";
+}
